fc4b4a82db0919cdb98a0c07cfd944fad5d16f34
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-03-02 14:58:12 +0400

    Add files via upload

---
+++ Payment_page/payment_page.js
@@ -0,0 +1,26 @@
+function validateAndSubmit() {
+    const cardNumber = document.getElementById('cardNumber').value;
+    const date = document.getElementById('date').value;
+    const cvv = document.getElementById('cvv').value;
+    const Code = document.getElementById('Code').value;
+
+    //stugenk vor dashtery datark chlinen
+    if (cardNumber === '' || date === '' || cvv === '' || Code === '') {
+        alert('Fields are empty');
+        return;
+    }
+    //stugenk vor qarti tvery linen valid
+    const Validcard = /^(?:\d{15}|\d{16})$/.test(cardNumber);
+    if (!Validcard) {
+        alert('Enter appropriate card number');
+        return;
+    }
+    const Validdate = /^(0[1-9]|1[0-2])\/\d{4}$/;
+    //stugenk vor amsativy lini valid
+    if (!Validdate.test(date)) {
+        alert('Enter appropriate date dd/mm/yy');
+        return;
+    }
+    
+    alert('Payment is done!');
+}
